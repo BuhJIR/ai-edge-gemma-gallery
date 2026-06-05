5a87e8f38344c043f85531ed3343e765defe28ba
BuhJIR <BuhJIR@users.noreply.github.com>
2026-06-05 09:17:43 +0300

    Unleash the Gemma: Kalash Mode

---
+++ Android/src/app/src/main/java/com/google/ai/edge/gallery/ui/theme/Color.kt
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.ai.edge.gallery.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Cyber Gothic Palette
+// Primary: Hot Pink/Magenta
+// Secondary: Cyan
+// Tertiary: Electric Purple
+// Background: Pure Black
+// Accents: Lime Green / Matrix Green
+
+val primaryLight = Color(0xFFFF006E)
+val onPrimaryLight = Color(0xFFFFFFFF)
+val primaryContainerLight = Color(0xFFFFD1E8)
+val onPrimaryContainerLight = Color(0xFF3B0014)
+val secondaryLight = Color(0xFF00B8D4)
+val onSecondaryLight = Color(0xFFFFFFFF)
+val secondaryContainerLight = Color(0xFFA7F1FF)
+val onSecondaryContainerLight = Color(0xFF001F25)
+val tertiaryLight = Color(0xFF9D00FF)
+val onTertiaryLight = Color(0xFFFFFFFF)
+val tertiaryContainerLight = Color(0xFFEEDBFF)
+val onTertiaryContainerLight = Color(0xFF2B0053)
+val errorLight = Color(0xFFB3261E)
+val onErrorLight = Color(0xFFFFFFFF)
+val errorContainerLight = Color(0xFFF9DEDC)
+val onErrorContainerLight = Color(0xFF8C1D18)
+val backgroundLight = Color(0xFF0D0208)
+val onBackgroundLight = Color(0xFF00FF41)
+val surfaceLight = Color(0xFF121212)
+val onSurfaceLight = Color(0xFF00FF41)
+val surfaceVariantLight = Color(0xFF262626)
+val onSurfaceVariantLight = Color(0xFF00F5FF)
+val surfaceContainerLowestLight = Color(0xFF000000)
+val surfaceContainerLowLight = Color(0xFF050505)
+val surfaceContainerLight = Color(0xFF0A0A0A)
+val surfaceContainerHighLight = Color(0xFF0F0F0F)
+val surfaceContainerHighestLight = Color(0xFF141414)
+val inverseSurfaceLight = Color(0xFFE3E3E3)
+val inverseOnSurfaceLight = Color(0xFF000000)
+val outlineLight = Color(0xFF00F5FF)
+val outlineVariantLight = Color(0xFF005F6B)
+val inversePrimaryLight = Color(0xFFFF006E)
+val surfaceDimLight = Color(0xFF0D0208)
+val surfaceBrightLight = Color(0xFF1A1A1A)
+val scrimLight = Color(0xFF000000)
+
+val primaryDark = Color(0xFFFF006E)
+val onPrimaryDark = Color(0xFF000000)
+val primaryContainerDark = Color(0xFF5A0020)
+val onPrimaryContainerDark = Color(0xFFFFD1E8)
+val secondaryDark = Color(0xFF00F5FF)
+val onSecondaryDark = Color(0xFF000000)
+val secondaryContainerDark = Color(0xFF004F58)
+val onSecondaryContainerDark = Color(0xFFA7F1FF)
+val tertiaryDark = Color(0xFF9D00FF)
+val onTertiaryDark = Color(0xFF000000)
+val tertiaryContainerDark = Color(0xFF450085)
+val onTertiaryContainerDark = Color(0xFFEEDBFF)
+val errorDark = Color(0xFFF2B8B5)
+val onErrorDark = Color(0xFF601410)
+val errorContainerDark = Color(0xFF8C1D18)
+val onErrorContainerDark = Color(0xFFF9DEDC)
+val backgroundDark = Color(0xFF000000)
+val onBackgroundDark = Color(0xFF00FF41)
+val surfaceDark = Color(0xFF0D0208)
+val onSurfaceDark = Color(0xFF00FF41)
+val surfaceVariantDark = Color(0xFF121212)
+val onSurfaceVariantDark = Color(0xFF00F5FF)
+val surfaceContainerLowestDark = Color(0xFF000000)
+val surfaceContainerLowDark = Color(0xFF050505)
+val surfaceContainerDark = Color(0xFF080808)
+val surfaceContainerHighDark = Color(0xFF0A0A0A)
+val surfaceContainerHighestDark = Color(0xFF0F0F0F)
+val inverseSurfaceDark = Color(0xFF00FF41)
+val inverseOnSurfaceDark = Color(0xFF000000)
+val outlineDark = Color(0xFF00F5FF)
+val outlineVariantDark = Color(0xFF005F6B)
+val inversePrimaryDark = Color(0xFFFF006E)
+val surfaceDimDark = Color(0xFF000000)
+val surfaceBrightDark = Color(0xFF1A1A1A)
+val scrimDark = Color(0xFF000000)
